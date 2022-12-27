9899137dc742c854571476267c810a23289cf893
DESKTOP\PC <2_D_Tkach@mail.ru>
2022-12-27 22:01:04 +0300

    Revert "поправил гитигнор"
    This reverts commit 801e6a62

--- MiniLocale/src/main/java/tkachgeek/config/minilocale/Message.java
+++ MiniLocale/src/main/java/tkachgeek/config/minilocale/Message.java
@@ -11,85 +11,85 @@ import tkachgeek.commands.command.arguments.executor.MessageReturn;
 import java.util.UUID;
 
 public class Message {
-    String message;
-
-    public Message() {
+  String message;
+  
+  public Message() {
+  }
+  
+  public Message(String message) {
+    this.message = message;
+  }
+  
+  public Message(String message, Mode mode) {
+    this.message = message;
+    
+    switch (mode) {
+      case MINI_MESSAGE:
+        break;
+      case LEGACY_AMPERSAND:
+        this.message = MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacyAmpersand().deserialize(message)).replaceAll("\\\\", "");
+        break;
+      case LEGACY_SECTION:
+        this.message = MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacySection().deserialize(message)).replaceAll("\\\\", "");
+        break;
     }
-
-    public Message(String message) {
-        this.message = message;
+  }
+  
+  public void send(Audience audience) {
+    audience.sendMessage(get());
+  }
+  
+  public void send(Audience audience, Placeholders placeholders) {
+    audience.sendMessage(get(placeholders));
+  }
+  
+  public void send(UUID maybeOfflinePlayer, Placeholders placeholders) {
+    if (Bukkit.getOfflinePlayer(maybeOfflinePlayer).isOnline()) {
+      Bukkit.getPlayer(maybeOfflinePlayer).sendMessage(get(placeholders));
     }
-
-    public Message(String message, Mode mode) {
-        this.message = message;
-
-        switch (mode) {
-            case MINI_MESSAGE:
-                break;
-            case LEGACY_AMPERSAND:
-                this.message = MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacyAmpersand().deserialize(message)).replaceAll("\\\\", "");
-                break;
-            case LEGACY_SECTION:
-                this.message = MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacySection().deserialize(message)).replaceAll("\\\\", "");
-                break;
-        }
+  }
+  
+  public void send(UUID maybeOfflinePlayer) {
+    if (Bukkit.getOfflinePlayer(maybeOfflinePlayer).isOnline()) {
+      Bukkit.getPlayer(maybeOfflinePlayer).sendMessage(get());
     }
-
-    public void send(Audience audience) {
-        audience.sendMessage(get());
+  }
+  
+  public void send(String playerName, Placeholders placeholders) {
+    Player player = Bukkit.getPlayer(playerName);
+    if (player != null) {
+      player.sendMessage(get(placeholders));
     }
-
-    public void send(Audience audience, Placeholders placeholders) {
-        audience.sendMessage(get(placeholders));
+  }
+  
+  public void send(String playerName) {
+    Player player = Bukkit.getPlayer(playerName);
+    if (player != null) {
+      player.sendMessage(get());
     }
-
-    public void send(UUID maybeOfflinePlayer, Placeholders placeholders) {
-        if (Bukkit.getOfflinePlayer(maybeOfflinePlayer).isOnline()) {
-            Bukkit.getPlayer(maybeOfflinePlayer).sendMessage(get(placeholders));
-        }
-    }
-
-    public void send(UUID maybeOfflinePlayer) {
-        if (Bukkit.getOfflinePlayer(maybeOfflinePlayer).isOnline()) {
-            Bukkit.getPlayer(maybeOfflinePlayer).sendMessage(get());
-        }
-    }
-
-    public void send(String playerName, Placeholders placeholders) {
-        Player player = Bukkit.getPlayer(playerName);
-        if (player != null) {
-            player.sendMessage(get(placeholders));
-        }
-    }
-
-    public void send(String playerName) {
-        Player player = Bukkit.getPlayer(playerName);
-        if (player != null) {
-            player.sendMessage(get());
-        }
-    }
-
-    public String getText() {
-        return LegacyComponentSerializer.legacySection().serialize(get());
-    }
-
-    public Component get() {
-        return MiniMessage.miniMessage().deserialize(message);
-    }
-
-    public Component get(Placeholders placeholders) {
-        return MiniMessage.miniMessage().deserialize(message, placeholders.getResolvers());
-    }
-
-    public void throwback() throws MessageReturn {
-        throw new MessageReturn(MiniMessage.miniMessage().deserialize(message));
-    }
-
-    public void throwback(Placeholders placeholders) throws MessageReturn {
-        throw new MessageReturn(MiniMessage.miniMessage().deserialize(message, placeholders.getResolvers()));
-    }
-
-    public boolean notEmpty() {
-        return !message.isEmpty();
-    }
+  }
+  
+  public String getText() {
+    return LegacyComponentSerializer.legacySection().serialize(get());
+  }
+  
+  public Component get() {
+    return MiniMessage.miniMessage().deserialize(message);
+  }
+  
+  public Component get(Placeholders placeholders) {
+    return MiniMessage.miniMessage().deserialize(message, placeholders.getResolvers());
+  }
+  
+  public void throwback() throws MessageReturn {
+    throw new MessageReturn(MiniMessage.miniMessage().deserialize(message));
+  }
+  
+  public void throwback(Placeholders placeholders) throws MessageReturn {
+    throw new MessageReturn(MiniMessage.miniMessage().deserialize(message, placeholders.getResolvers()));
+  }
+  
+  public boolean notEmpty() {
+    return !message.isEmpty();
+  }
 }
