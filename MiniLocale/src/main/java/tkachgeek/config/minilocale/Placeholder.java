b17f36795fb7b9cd7c3d67dac65140aa784f0ae3
Soul_KRT <soul.krt.00@gmail.com>
2022-12-27 21:53:34 +0300

    add overload to Placeholder add method

--- MiniLocale/src/main/java/tkachgeek/config/minilocale/Placeholder.java
+++ MiniLocale/src/main/java/tkachgeek/config/minilocale/Placeholder.java
@@ -1,11 +1,15 @@
 package tkachgeek.config.minilocale;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
 public class Placeholder {
     public static Placeholders add(String key, String value) {
         return new Placeholders(TagResolver.resolver(key, Tag.preProcessParsed(value)));
+    }
+    public static Placeholders add(String key, Component value) {
+        return new Placeholders(TagResolver.resolver(key, Tag.inserting(value)));
     }
 
     public static Placeholders add(TagResolver resolver) {
