d43fbb138fff7077cc16aa07aef29cd14d418d38
DESKTOP\PC <2_D_Tkach@mail.ru>
2022-12-28 14:37:42 +0300

    Revert "для того, чтобы не ругалось в compile-time"
    This reverts commit 1bd85007e754fb545c5b825c1a8bb13914d98f23.

--- MiniLocale/src/main/java/tkachgeek/commands/command/arguments/executor/MessageReturn.java
+++ MiniLocale/src/main/java/tkachgeek/commands/command/arguments/executor/MessageReturn.java
@@ -1,10 +1,5 @@
 package tkachgeek.commands.command.arguments.executor;
 
-import net.kyori.adventure.text.Component;
-
 public class MessageReturn extends Exception {
-  
-  public MessageReturn(Component ignored) {
-  
-  }
+
 }
