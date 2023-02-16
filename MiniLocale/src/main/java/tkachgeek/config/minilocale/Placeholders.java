da958443bd51643c0ef117a6741bee25dfcca199
DESKTOP\PC <2_D_Tkach@mail.ru>
2023-02-16 14:41:07 +0300

    Placeholders:add теперь замещает существующий плейсхолдер (по ключу)

--- MiniLocale/src/main/java/tkachgeek/config/minilocale/Placeholders.java
+++ MiniLocale/src/main/java/tkachgeek/config/minilocale/Placeholders.java
@@ -3,57 +3,57 @@ package tkachgeek.config.minilocale;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import tkachgeek.tkachutils.text.nanoid.NanoID;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class Placeholders {
-    private final List<TagResolver> resolvers = new ArrayList<>();
-
-    public Placeholders(String key, String value) {
-        add(key, value);
-    }
-    
-    public Placeholders(TagResolver resolver) {
-        add(resolver);
-    }
-    
-    public Placeholders add(String key, String value) {
-        resolvers.add(TagResolver.resolver(key.toLowerCase(), Tag.preProcessParsed(value)));
-        return this;
-    }
-    
-    public Placeholders add(String key, double value) {
-        return add(key, String.valueOf(value));
-    }
-    
-    public Placeholders add(String key, int value) {
-        return add(key, String.valueOf(value));
-    }
-    
-    public Placeholders add(String key, float value) {
-        return add(key, String.valueOf(value));
-    }
-    
-    public Placeholders add(String key, long value) {
-        return add(key, String.valueOf(value));
-    }
-    
-    public Placeholders add(String key, boolean value) {
-        return add(key, String.valueOf(value));
-    }
-    
-    public Placeholders add(TagResolver TagResolver) {
-        resolvers.add(TagResolver);
-        return this;
-    }
-    
-    public Placeholders add(String key, Component value) {
-        resolvers.add(TagResolver.resolver(key.toLowerCase(), Tag.inserting(value)));
-        return this;
-    }
-    
-    public TagResolver[] getResolvers() {
-        return resolvers.toArray(new TagResolver[0]);
-    }
+  private final HashMap<String, TagResolver> resolvers = new HashMap<>();
+  
+  public Placeholders(String key, String value) {
+    add(key, value);
+  }
+  
+  public Placeholders(TagResolver resolver) {
+    add(resolver);
+  }
+  
+  public Placeholders add(String key, String value) {
+    resolvers.put(key, TagResolver.resolver(key.toLowerCase(), Tag.preProcessParsed(value)));
+    return this;
+  }
+  
+  public Placeholders add(String key, double value) {
+    return add(key, String.valueOf(value));
+  }
+  
+  public Placeholders add(String key, int value) {
+    return add(key, String.valueOf(value));
+  }
+  
+  public Placeholders add(String key, float value) {
+    return add(key, String.valueOf(value));
+  }
+  
+  public Placeholders add(String key, long value) {
+    return add(key, String.valueOf(value));
+  }
+  
+  public Placeholders add(String key, boolean value) {
+    return add(key, String.valueOf(value));
+  }
+  
+  public Placeholders add(TagResolver tagResolver) {
+    resolvers.put(NanoID.randomNanoId(), tagResolver);
+    return this;
+  }
+  
+  public Placeholders add(String key, Component value) {
+    resolvers.put(key, TagResolver.resolver(key.toLowerCase(), Tag.inserting(value)));
+    return this;
+  }
+  
+  public TagResolver[] getResolvers() {
+    return resolvers.values().toArray(new TagResolver[0]);
+  }
 }
