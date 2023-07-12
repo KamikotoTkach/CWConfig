7b7c16519e3c8280568833d740504f0ac64c45ed
DESKTOP\PC <danil2002tkachenko@gmail.com>
2023-07-12 12:45:09 +0300

    MiniMessageTestCommand +относительно кого выполнить, + с плейсхолдерами

--- MiniLocale/src/main/java/tkachgeek/config/minilocale/MiniMessageTestCommand.java
+++ MiniLocale/src/main/java/tkachgeek/config/minilocale/MiniMessageTestCommand.java
@@ -1,23 +1,24 @@
 package tkachgeek.config.minilocale;
 
+import org.bukkit.Bukkit;
 import tkachgeek.commands.command.ArgumentSet;
 import tkachgeek.commands.command.Command;
+import tkachgeek.commands.command.arguments.bukkit.OnlinePlayers;
 import tkachgeek.commands.command.arguments.executor.Executor;
 import tkachgeek.commands.command.arguments.spaced.SpacedStringArg;
-import tkachgeek.config.minilocale.wrapper.adventure.MiniMessageWrapper;
 import tkachgeek.tkachutils.messages.MessageReturn;
 
 public class MiniMessageTestCommand {
   public static Command get() {
     return new Command("miniMessageTest").arguments(
-       new ArgumentSet(new MiniMessageTestExecutor(), "*", new SpacedStringArg("text"))
+       new ArgumentSet(new MiniMessageTestExecutor(), "*", new OnlinePlayers(), new SpacedStringArg("text"))
     );
   }
   
   private static class MiniMessageTestExecutor extends Executor {
     @Override
     public void executeForPlayer() throws MessageReturn {
-      throw new MessageReturn(MiniMessageWrapper.deserialize(argS(0)));
+      sender.sendMessage(new Message(argS(1)).get(Bukkit.getPlayer(argS(0))));
     }
   }
 }
