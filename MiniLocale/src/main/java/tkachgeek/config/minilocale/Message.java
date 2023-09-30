ad737f96b0cdb79cf6d2efcc16d34e13999059e9
Soul_KRT <soul.krt.00@gmail.com>
2023-09-30 13:30:02 +0300

    add parse method in Message

--- MiniLocale/src/main/java/tkachgeek/config/minilocale/Message.java
+++ MiniLocale/src/main/java/tkachgeek/config/minilocale/Message.java
@@ -13,21 +13,23 @@ import tkachgeek.config.minilocale.wrapper.adventure.MiniMessageWrapper;
 import tkachgeek.config.minilocale.wrapper.papi.PapiWrapper;
 import tkachgeek.tkachutils.collections.CollectionUtils;
 import tkachgeek.tkachutils.messages.TargetableMessageReturn;
+import tkachgeek.tkachutils.text.component.LegacyComponentUtil;
 
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class Message implements Serializable {
   protected String message;
-  
+
   public Message() {
   }
-  
+
   public Message(String... message) {
     this(CollectionUtils.toString(message, "", "\n", true));
   }
-  
+
   public Message(String message) {
     this.message = message;
   }
@@ -35,30 +37,30 @@ public class Message implements Serializable {
   public Message(Component message) {
     this.message = LegacyComponentSerializer.legacySection().serialize(message);
   }
-  
+
   public Message(Mode mode, String... message) {
     this(CollectionUtils.toString(message, "", "\n", true), mode);
   }
-  
+
   public Message(String message, Mode mode) {
     this.message = message;
-    
+
     switch (mode) {
       case MINI_MESSAGE:
         break;
       case LEGACY_AMPERSAND:
-        this.message = MiniMessageWrapper.serialize(LegacyComponentSerializer.legacyAmpersand().deserialize(message)).replaceAll("\\\\", "");
+        this.message = MiniMessageWrapper.serialize(LegacyComponentUtil.parse(message)).replaceAll("\\\\", "");
         break;
       case LEGACY_SECTION:
         this.message = MiniMessageWrapper.serialize((LegacyComponentSerializer.legacySection().deserialize(message))).replaceAll("\\\\", "");
         break;
     }
   }
-  
+
   public void sendActionBar(Audience audience) {
     send(ActionBarDirection.INSTANCE, audience);
   }
-  
+
   public void send(MessageDirection direction, Audience audience) {
     audience.forEachAudience(x -> {
       if (x instanceof CommandSender) {
@@ -68,15 +70,15 @@ public class Message implements Serializable {
       }
     });
   }
-  
+
   public void sendActionBar(Audience audience, Placeholders placeholders) {
     send(ActionBarDirection.INSTANCE, audience, placeholders);
   }
-  
+
   public void sendActionBar(Iterator<? extends Audience> audiences, Placeholders placeholders) {
     send(ActionBarDirection.INSTANCE, audiences, placeholders);
   }
-  
+
   public void send(MessageDirection direction, Iterator<? extends Audience> audiences, Placeholders placeholders) {
     audiences.forEachRemaining(audience -> audience.forEachAudience(item -> {
       if (audience instanceof CommandSender) {
@@ -86,7 +88,7 @@ public class Message implements Serializable {
       }
     }));
   }
-  
+
   public void send(MessageDirection direction, Audience audience, Placeholders placeholders) {
     audience.forEachAudience(x -> {
       if (x instanceof CommandSender) {
@@ -96,19 +98,19 @@ public class Message implements Serializable {
       }
     });
   }
-  
+
   public Component get(Placeholders placeholders, CommandSender receiver) {
     return MiniMessageWrapper.deserialize(PapiWrapper.process(message, receiver), placeholders);
   }
-  
+
   public Component get(Placeholders placeholders) {
     return MiniMessageWrapper.deserialize(message, placeholders);
   }
-  
+
   public void sendActionBar(Iterator<? extends Audience> audiences) {
     send(ActionBarDirection.INSTANCE, audiences);
   }
-  
+
   public void send(MessageDirection direction, Iterator<? extends Audience> audiences) {
     audiences.forEachRemaining(audience -> audience.forEachAudience(item -> {
       if (audience instanceof CommandSender) {
@@ -118,158 +120,179 @@ public class Message implements Serializable {
       }
     }));
   }
-  
+
   public Component get(CommandSender receiver) {
     return MiniMessageWrapper.deserialize(PapiWrapper.process(message, receiver));
   }
-  
+
   public Component get() {
     return MiniMessageWrapper.deserialize(message);
   }
-  
+
   public void sendTitle(Audience audience) {
     send(TitleDirection.INSTANCE, audience);
   }
-  
+
   public void sendTitle(Audience audience, Placeholders placeholders) {
     send(TitleDirection.INSTANCE, audience, placeholders);
   }
-  
+
   public void sendTitle(Iterator<? extends Audience> audiences) {
     send(TitleDirection.INSTANCE, audiences);
   }
-  
+
   public void sendTitle(Iterator<? extends Audience> audiences, Placeholders placeholders) {
     send(TitleDirection.INSTANCE, audiences, placeholders);
   }
-  
+
   public void sendSubtitle(Audience audience) {
     send(SubtitleDirection.INSTANCE, audience);
   }
-  
+
   public void sendSubtitle(Audience audience, Placeholders placeholders) {
     send(SubtitleDirection.INSTANCE, audience, placeholders);
   }
-  
+
   public void sendSubtitle(Iterator<? extends Audience> audiences) {
     send(SubtitleDirection.INSTANCE, audiences);
   }
-  
+
   public void sendSubtitle(Iterator<? extends Audience> audiences, Placeholders placeholders) {
     send(SubtitleDirection.INSTANCE, audiences, placeholders);
   }
-  
+
   public void broadcast(MessageDirection direction) {
     send(direction, AudienceWrapper.onlinePlayers());
   }
-  
+
   public void broadcast(MessageDirection direction, Placeholders placeholders) {
     send(direction, AudienceWrapper.onlinePlayers(), placeholders);
   }
-  
+
   public void broadcast() {
     send(ChatDirection.INSTANCE, AudienceWrapper.onlinePlayers());
   }
-  
+
   public void send(UUID maybeOfflinePlayer, Placeholders placeholders) {
     if (Bukkit.getPlayer(maybeOfflinePlayer) != null) {
       send(Bukkit.getPlayer(maybeOfflinePlayer), placeholders);
     }
   }
-  
+
   public void send(Audience audience, Placeholders placeholders) {
     send(ChatDirection.INSTANCE, audience, placeholders);
   }
-  
+
   public void send(UUID maybeOfflinePlayer) {
     if (Bukkit.getPlayer(maybeOfflinePlayer) != null) {
       send(Bukkit.getPlayer(maybeOfflinePlayer));
     }
   }
-  
+
   public void send(Audience audience) {
     send(ChatDirection.INSTANCE, audience);
   }
-  
+
   public void send(String playerName, Placeholders placeholders) {
     Player player = Bukkit.getPlayer(playerName);
     if (player != null) {
       send(player, placeholders);
     }
   }
-  
+
   public void send(String playerName) {
     Player player = Bukkit.getPlayer(playerName);
     if (player != null) {
       send(player);
     }
   }
-  
+
   public String getLegacy() {
     return LegacyComponentSerializer.legacyAmpersand().serialize(get());
   }
-  
+
   public String getLegacy(Placeholders placeholders) {
     return LegacyComponentSerializer.legacyAmpersand().serialize(get(placeholders));
   }
-  
+
   public String getLegacy(CommandSender receiver) {
     return LegacyComponentSerializer.legacyAmpersand().serialize(get(receiver));
   }
-  
+
   public String getLegacy(Placeholders placeholders, CommandSender receiver) {
     return LegacyComponentSerializer.legacyAmpersand().serialize(get(placeholders, receiver));
   }
-  
+
   public String getLegacySection() {
     return LegacyComponentSerializer.legacySection().serialize(get());
   }
-  
+
   public String getLegacySection(Placeholders placeholders) {
     return LegacyComponentSerializer.legacySection().serialize(get(placeholders));
   }
-  
+
   public String getLegacySection(CommandSender receiver) {
     return LegacyComponentSerializer.legacySection().serialize(get(receiver));
   }
-  
+
   public String getLegacySection(Placeholders placeholders, CommandSender receiver) {
     return LegacyComponentSerializer.legacySection().serialize(get(placeholders, receiver));
   }
-  
+
   public void throwback() throws TargetableMessageReturn {
     throw new TargetableMessageReturn(this::get);
   }
-  
+
   public void throwback(Placeholders placeholders) throws TargetableMessageReturn {
     throw new TargetableMessageReturn(receiver -> get(placeholders, receiver));
   }
-  
+
   public boolean isNotEmpty() {
     return !message.isEmpty();
   }
-  
+
   public boolean isEmpty() {
     return message.isEmpty();
   }
-  
+
   public String getText() {
     return PlainTextComponentSerializer.plainText().serialize(get());
   }
-  
+
   public String getText(CommandSender receiver) {
     return PlainTextComponentSerializer.plainText().serialize(get(receiver));
   }
-  
+
   public String getText(Placeholders placeholders) {
     return PlainTextComponentSerializer.plainText().serialize(get(placeholders));
   }
-  
+
   public String getText(Placeholders placeholders, CommandSender receiver) {
     return PlainTextComponentSerializer.plainText().serialize(get(placeholders, receiver));
   }
-  
+
   public String serialize() {
     return message;
+  }
+
+  public static Message parse(String message) {
+    Mode mode;
+    if (isAmpersand(message)) {
+      mode = Mode.LEGACY_AMPERSAND;
+    } else if (isSection(message)) {
+      mode = Mode.LEGACY_SECTION;
+    } else {
+      mode = Mode.MINI_MESSAGE;
+    }
+
+    return new Message(message, mode);
+  }
+
+  private static boolean isAmpersand(String message) {
+    return Pattern.compile("&[\\d#abcdefklmnrx]").matcher(message).find();
+  }
+
+  private static boolean isSection(String message) {
+    return Pattern.compile("§[\\d#abcdefklmnrx]").matcher(message).find();
   }
 }
