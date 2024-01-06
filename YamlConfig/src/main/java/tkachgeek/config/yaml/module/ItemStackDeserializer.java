6cbfb27cf720c0509e7a5adf560c9e194bf3807d
DESKTOP\PC <tkach@cwcode.ru>
2024-01-06 21:49:52 +0300

    item deserializer fix

--- YamlConfig/src/main/java/tkachgeek/config/yaml/module/ItemStackDeserializer.java
+++ YamlConfig/src/main/java/tkachgeek/config/yaml/module/ItemStackDeserializer.java
@@ -8,12 +8,21 @@ import org.bukkit.inventory.ItemStack;
 import tkachgeek.tkachutils.items.ItemStackUtils;
 
 import java.io.IOException;
+import java.util.Base64;
 
 public class ItemStackDeserializer extends JsonDeserializer<ItemStack> {
   
   @Override
   public ItemStack deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-    if (p.hasToken(JsonToken.VALUE_STRING)) return ItemStackUtils.fromSNBT(p.getValueAsString());
+    if (p.hasToken(JsonToken.VALUE_STRING)) {
+      String str = p.getValueAsString();
+      
+      if (!str.contains("{")) {
+        return ItemStack.deserializeBytes(Base64.getDecoder().decode(str));
+      }
+      
+      return ItemStackUtils.fromSNBT(str);
+    }
     
     return ItemStack.deserializeBytes(p.getBinaryValue());
   }
