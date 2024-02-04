5d6af172b5e0fd6aae5be3a447a64b47b175df57
DESKTOP\PC <tkach@cwcode.ru>
2024-02-04 20:10:16 +0300

    de/serializer fix

--- YamlConfig/src/main/java/tkachgeek/config/yaml/YmlConfigManager.java
+++ YamlConfig/src/main/java/tkachgeek/config/yaml/YmlConfigManager.java
@@ -90,9 +90,9 @@ public class YmlConfigManager {
     
     module.addDeserializer(MessageArr.class, new MessageArrDeserializer());
     module.addSerializer(MessageArr.class, new MessageArrSerializer());
-
-    module(new JacksonPaperWithoutItemStack());
+    
     module(module);
+    module(new JacksonPaperWithoutItemStack());
   }
   
   public void module(Module module) {
