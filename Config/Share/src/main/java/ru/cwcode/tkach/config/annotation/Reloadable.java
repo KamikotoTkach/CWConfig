96c060a66ef66214614e6831462f674c88b4ab60
DESKTOP\PC <tkach@cwcode.ru>
2024-08-15 17:33:18 +0300

    Reloadable interface wrong package comment

--- Config/Share/src/main/java/ru/cwcode/tkach/config/annotation/Reloadable.java
+++ Config/Share/src/main/java/ru/cwcode/tkach/config/annotation/Reloadable.java
@@ -1,5 +1,6 @@
 package ru.cwcode.tkach.config.annotation;
 
+//Yes, it's in the wrong package, but if I move it, it breaks all the dependent plugins :(
 public interface Reloadable {
   boolean reload();
 }
