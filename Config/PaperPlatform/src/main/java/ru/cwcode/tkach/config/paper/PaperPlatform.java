06eee8f01cc47aeaad6b4cdccf5b49de216b6ce7
DESKTOP\PC <tkach@cwcode.ru>
2024-08-15 17:27:22 +0300

    init l10n platform

--- Config/PaperPlatform/src/main/java/ru/cwcode/tkach/config/paper/PaperPlatform.java
+++ Config/PaperPlatform/src/main/java/ru/cwcode/tkach/config/paper/PaperPlatform.java
@@ -1,7 +1,11 @@
 package ru.cwcode.tkach.config.paper;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.cwcode.cwutils.l10n.L10n;
+import ru.cwcode.cwutils.l10n.PaperL10nPlatform;
+import ru.cwcode.cwutils.server.PaperServerUtils;
 import ru.cwcode.cwutils.server.ServerUtils;
+import ru.cwcode.tkach.config.server.ServerPlatform;
 import ru.cwcode.tkach.locale.paper.modern.MiniLocaleNew;
 import ru.cwcode.tkach.locale.paper.old.MiniLocaleOld;
 import ru.cwcode.tkach.locale.platform.MiniLocale;
@@ -9,7 +13,9 @@ import ru.cwcode.tkach.locale.platform.MiniLocale;
 public final class PaperPlatform extends JavaPlugin {
   @Override
   public void onEnable() {
-    if (ServerUtils.isVersionGreater("1.17.1")) {
+    ServerPlatform.l10n = new L10n(new PaperL10nPlatform(this, this.getFile()));
+    
+    if (PaperServerUtils.isVersionGreater("1.17.1")) {
       MiniLocale.setInstance(new MiniLocaleNew());
     } else {
       MiniLocale.setInstance(new MiniLocaleOld());
--- Config/VelocityPlatform/src/main/java/ru/cwcode/tkach/config/velocityplatform/VelocityPlatform.java
+++ Config/VelocityPlatform/src/main/java/ru/cwcode/tkach/config/velocityplatform/VelocityPlatform.java
@@ -5,22 +5,45 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+import ru.cwcode.cwutils.l10n.L10n;
+import ru.cwcode.cwutils.l10n.VelocityL10nPlatform;
+import ru.cwcode.tkach.config.server.ServerPlatform;
 import ru.cwcode.tkach.locale.platform.MiniLocale;
 import ru.cwcode.tkach.locale.velocity.MiniLocaleVelocity;
 
+import java.nio.file.Path;
+
 @Plugin(
-   id = "cwconfig",
-   name = "CWConfig",
-   version = "2.1.3",
-   dependencies = {@Dependency(id = "cwcommands")}
+  id = "cwconfig",
+  name = "CWConfig",
+  version = "2.1.3",
+  dependencies = {@Dependency(id = "cwcommands")}
 )
 public class VelocityPlatform {
   @Inject
   ProxyServer proxyServer;
+  @Inject
+  @DataDirectory
+  Path dataDirectory;
+  @Inject
+  Logger logger;
+  @Inject
+  ProxyServer server;
   
   @Subscribe
   public void initialize(ProxyInitializeEvent event) {
+    ServerPlatform.l10n = new L10n(new VelocityL10nPlatform(this,
+                                                            dataDirectory,
+                                                            logger,
+                                                            server.getPluginManager()
+                                                                  .ensurePluginContainer(this)
+                                                                  .getDescription()
+                                                                  .getSource()
+                                                                  .orElseThrow()
+                                                                  .toFile()));
     MiniLocale.setInstance(new MiniLocaleVelocity(proxyServer));
   }
 }
