95a747c833cfede669860c7e04c8f1022a2e5b5a
Danil Tkach <tkach@cwcode.ru>
2024-11-04 17:38:11 +0300

    add constructors to ConfigManagers

--- Config/Share/src/main/java/ru/cwcode/tkach/config/jackson/JacksonConfigManager.java
+++ Config/Share/src/main/java/ru/cwcode/tkach/config/jackson/JacksonConfigManager.java
@@ -5,6 +5,9 @@ import ru.cwcode.tkach.config.base.ConfigPlatform;
 import ru.cwcode.tkach.config.base.manager.*;
 
 public abstract class JacksonConfigManager<C extends Config<C>> extends ConfigManager<C> {
+  public JacksonConfigManager(ConfigPlatform configPlatform, ConfigLoader<C> loader, ConfigPersister<C> persister, ConfigMapper<C> mapper, ConfigCreator<C> creator) {
+    super(configPlatform, loader, persister, mapper, creator);
+  }
   
   public JacksonConfigManager(ConfigPlatform configPlatform, ConfigPersister<C> persister, ConfigMapper<C> mapper) {
     super(configPlatform, new ConfigLoader<>(), persister, mapper, new ConfigCreator<>());
--- Config/Share/src/main/java/ru/cwcode/tkach/config/jackson/yaml/YmlConfigManager.java
+++ Config/Share/src/main/java/ru/cwcode/tkach/config/jackson/yaml/YmlConfigManager.java
@@ -3,6 +3,9 @@ package ru.cwcode.tkach.config.jackson.yaml;
 import ru.cwcode.cwutils.collections.CollectionUtils;
 import ru.cwcode.tkach.config.annotation.Description;
 import ru.cwcode.tkach.config.base.ConfigPlatform;
+import ru.cwcode.tkach.config.base.manager.ConfigCreator;
+import ru.cwcode.tkach.config.base.manager.ConfigLoader;
+import ru.cwcode.tkach.config.base.manager.ConfigMapper;
 import ru.cwcode.tkach.config.base.manager.ConfigPersister;
 import ru.cwcode.tkach.config.jackson.JacksonConfigManager;
 
@@ -11,11 +14,28 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class YmlConfigManager extends JacksonConfigManager<YmlConfig> {
+  public YmlConfigManager(ConfigPlatform configPlatform,
+                          ConfigLoader<YmlConfig> loader,
+                          ConfigPersister<YmlConfig> persister,
+                          ConfigMapper<YmlConfig> mapper,
+                          ConfigCreator<YmlConfig> creator) {
+    super(configPlatform, loader, persister, mapper, creator);
+    
+    persister.setPreprocessor(getConfigPreprocessor());
+  }
+  
+  public YmlConfigManager(ConfigPlatform configPlatform, ConfigCreator<YmlConfig> creator) {
+    this(configPlatform, new ConfigLoader<>(), new ConfigPersister<>(), new YmlConfigMapper(), creator);
+  }
   
   public YmlConfigManager(ConfigPlatform configPlatform) {
     super(configPlatform, new ConfigPersister<>(), new YmlConfigMapper());
     
-    persister.setPreprocessor(new ConfigPersister.Preprocessor<>() {
+    persister.setPreprocessor(getConfigPreprocessor());
+  }
+  
+  private ConfigPersister.Preprocessor<YmlConfig> getConfigPreprocessor() {
+    return new ConfigPersister.Preprocessor<>() {
       @Override
       public String preprocess(YmlConfig config, String data) {
         String[] header = config.header();
@@ -34,6 +54,6 @@ public class YmlConfigManager extends JacksonConfigManager<YmlConfig> {
         
         return data;
       }
-    });
+    };
   }
 }
