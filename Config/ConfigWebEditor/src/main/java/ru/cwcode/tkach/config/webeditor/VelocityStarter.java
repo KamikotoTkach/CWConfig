9cd87ebc95e5aed42bb9642321b292e3dde24896
Soul_KRT <soul.krt.00@gmail.com>
2025-08-14 04:37:04 +0300

    bump version to 2.4.2 - update libs versions - fix maven central deployment

--- Config/ConfigWebEditor/src/main/java/ru/cwcode/tkach/config/webeditor/VelocityStarter.java
+++ Config/ConfigWebEditor/src/main/java/ru/cwcode/tkach/config/webeditor/VelocityStarter.java
@@ -16,7 +16,7 @@ import java.nio.file.Path;
 @Plugin(
   id = "cwconfig_webeditor",
   name = "CWConfig web editor",
-  version = "2.4.1",
+  version = "2.4.2",
   dependencies = {@Dependency(id = "cwconfig")}
 )
 public class VelocityStarter {
