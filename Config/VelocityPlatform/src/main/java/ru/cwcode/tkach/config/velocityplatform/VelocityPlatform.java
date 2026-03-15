7ac742d68eaad9ac4098c66346c167ffd7ccf246
Danil Tkach <tkach@cwcode.ru>
2026-03-15 23:31:25 +0300

    v2.6.1

--- Config/VelocityPlatform/src/main/java/ru/cwcode/tkach/config/velocityplatform/VelocityPlatform.java
+++ Config/VelocityPlatform/src/main/java/ru/cwcode/tkach/config/velocityplatform/VelocityPlatform.java
@@ -19,7 +19,7 @@ import java.nio.file.Path;
 @Plugin(
   id = "cwconfig",
   name = "CWConfig",
-  version = "2.6.0",
+  version = "2.6.1",
   dependencies = {@Dependency(id = "cwcommands")}
 )
 public class VelocityPlatform {
